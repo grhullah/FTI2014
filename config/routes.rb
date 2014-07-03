4dc1d70f9a3cb4fc73699694c1b69050240d5276
Gary R. Hullah <grhullah@gmail.com>
2014-07-03 11:35:05 -0400

    Created About Page and Nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get "about" => "pages#about"
+  
+  
+
+  # Add      get "about" => "pages#about"
+  # for each page added
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
